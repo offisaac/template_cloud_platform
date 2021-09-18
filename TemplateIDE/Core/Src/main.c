f0cd36b2507faaabd0da467b15bbe55580193478
Lu-Lutoo <e19135693@163.com>
2021-09-19 01:06:29 +0800

    加入老旧上位机和SystemView

--- TemplateIDE/Core/Src/main.c
+++ TemplateIDE/Core/Src/main.c
@@ -108,7 +108,7 @@ int main(void)
   MX_USART1_UART_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
-
+  App_Device_Init();
   /* USER CODE END 2 */
 
   /* USER CODE BEGIN RTOS_MUTEX */
