4b95afa1609e4e5b08d07f038c3c46d02b5ce8e2
kk_fun <2450690281@qq.com>
2021-11-29 23:11:56 +0800

    first commit

--- Template4MSVC_WIN32/Usr/simulation.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <System_DataPool.h>
-#include <CoppeliaSim.h>
-extern CoppeliaSim_Client CoppeliaSim;
-extern TaskHandle_t Sim_Handle;
-
-void Task_Sim(void* arg);
-void Service_CoppeliaSim_Init();
